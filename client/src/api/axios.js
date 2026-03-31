1ff794fd118d9b43bdf98c9cc21ba875ada905e7
Harini <harinimuthu0305@gmail.com>
2026-03-31 12:04:59 +0530

    fix cors error

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:"beauty-store-products-production.up.railway.app/api",
   withCredentials: true
 });
 
